1e1c62f4c7f80f4a18b969790be1da1bc57fa54b
Chiara <souliechiara@gmail.com>
2024-02-24 12:18:48 +0100

    debut design

--- src/components/Frigo.vue
+++ src/components/Frigo.vue
@@ -1,36 +1,111 @@
 <script setup>
 
+   
+
+
 </script>
 
 <template>
-    <div class="porte"></div>
-    
-
+  <div class="kitchen">
+    <div class="fridge">
+      <div class="door"></div>
+      <div class="poignee_haut"></div>
+      <div class="poignee_basse"></div>
+      <div class="divider"></div> 
+    </div>
+    <div class="meubles">
+      <div class="meubled"></div>
+      <div class="meubleg"></div>
+      <div class="hotte"></div>
+      <div class="hotte2"></div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-h1{
-  text-align: center;
-  font-family: "times", cursive;
-  color: rgb(32, 27, 27);
+.kitchen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
-p{
-  text-align: center;
-  font-family: "times", cursive;
-  color: rgb(32, 27, 27);
-}
-.porte {
-  background-color: rgba(88, 99, 90, 0.712);
-  position: center;
+
+.fridge {
   width: 500px;
   min-height: 700px;
-  height: auto;
-  margin: auto;
-  border-radius: 10px;
-  padding: 1px 10px;
-  transform-origin: left;
-  /*Speed of the Door animation*/
-  transition: all 1s ease-in-out;
+  background-color: gray;
+  position: relative;
+}
+
+.door {
+  width: 10%;
+  height: 100%;
+  background-color: #fff;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+.poignee_haut{
+  width: 10px;
+  height: 110px;
+  right:80px;
+  background-color: black;
+  position: absolute;
+  top: 25%;
+}
+.poignee_basse{
+  width: 10px;
+  height: 90px;
+  right: 80px;
+  background-color: black;
+  position: absolute;
+  top: 60%;
+}
+
+.divider {
+  width: 100%; /* Largeur du trait de séparation */
+  height: 2px; /* Épaisseur du trait */
+  background-color: #fff;
+  position: absolute;
+  top: 50%; /* Pour diviser le frigo en deux parties */
+  left: 0;
+}
+.meubled{
+  width: 500px;
+  height: 300px;
+  right: 10px;
+  background-color: rgb(82, 75, 75);
+  position:absolute;
+  top: 58.75%; 
+}
+
+.meubleg{
+  width: 400px;
+  height: 300px;
+  left: 60px;
+  background-color: rgb(82, 75, 75);
+  position:absolute;
+  top: 58.75%; 
+}
+
+.hotte{
+  width: 50px; /* Largeur de la hotte */
+  height: 200px; /* Hauteur de la hotte */
+  background-color: silver; /* Couleur de la hotte */
+  position: absolute;
+  top: 15%; /* Position verticale au centre */
+  left: 80%; /* Positionnement à droite du frigo */
+  border-radius: 10px; /* Pour arrondir les coins */
+}
+
+.hotte2{
+  width: 300px; /* Largeur de la hotte */
+  height: 20px; /* Hauteur de la hotte */
+  background-color: silver; /* Couleur de la hotte */
+  position: absolute;
+  top: 32%; /* Position verticale au centre */
+  left: 72%; /* Positionnement à droite du frigo */
+  border-radius: 10px; /* Pour arrondir les coins */
 }
 
 </style>
--- src/pages/PageAccueil.vue
+++ src/pages/PageAccueil.vue
@@ -4,6 +4,7 @@
       <h1>Bienvenue dans le frigo de Chiara</h1><br>
       <p>Appuyez sur la poignée pour l'ouvrir</p><br>
     </div>
+    <Frigo></Frigo>
     <v-btn to="/PageListe" class="ouvrir">Appuyez ici</v-btn>
   </body>
 </template>
@@ -17,12 +18,5 @@
   position: relative;
   bottom: 500px;
   left: 190px;
-}
-body{
-    background-image: url('../assets/lacuisine.avif');
-    /* Autres propriétés de style */
-    background-size: cover; /* Ajuste la taille de l'image pour couvrir toute la zone */
-    background-repeat: no-repeat; /* Empêche la répétition de l'image */
-    min-height: 100vh;
 }
 </style>
